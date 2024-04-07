6056e1eb4d3e4a976edeb91a8ef0736465cf30c6
Rohitash Kumar <rohit77428@gmail.com>
2024-04-07 23:39:52 +0530

    middlewares for ptotected route
    set middlewares for  protected route
    with a token how a user can access protected data or routes
    
    Co-Authored-By: Narayan Ram Bhambhu <97403098+narayanbhambhu25@users.noreply.github.com>
    Co-Authored-By: Amankumar1811 <164324447+Amankumar1811@users.noreply.github.com>

--- server/keys.js
+++ server/keys.js
@@ -1,3 +1,4 @@
 module.exports={
-    MONGOURI:"mongodb+srv://rohitash:zrV00XU3xby6khZr@cluster0.ymxenhj.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+    MONGOURI:"mongodb+srv://rohitash:zrV00XU3xby6khZr@cluster0.ymxenhj.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0",
+    JWT_SECRET:"abcdjflkgjnfkdsn"
 }
--- server/routes/auth.js
+++ server/routes/auth.js
@@ -3,8 +3,14 @@ const router = express.Router();
 const mongoose = require("mongoose");
 const User  = mongoose.model("User");
 const bcrypt = require("bcryptjs");
+const jwt =  require("jsonwebtoken");
+const {JWT_SECRET} = require("../keys");
+const requirelogin = require("../middleware/requirelogin");
 
 
+router.get("/protected",requirelogin,(req,res)=>{
+  res.send("hello user");
+})
 
 router.post("/signup",(req,res)=>{
   const {name,email,password} = req.body
@@ -62,7 +68,9 @@ router.post("/signin",(req,res)=>{
     .then(doMatch=>{
       if(doMatch)
       {
-      res.json({message:"user saved successfully"})
+      // res.json({message:"user saved successfully"})
+      const token = jwt.sign({_id:savedUser._id},JWT_SECRET);
+       res.json({token:token});
       }
       else{
         return res.status(422).json({error:"invalid email or password"})
