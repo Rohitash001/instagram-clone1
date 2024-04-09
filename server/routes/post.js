6dc014ade619f7e58c7e92dfd8a400cd488f1391
Rohitash Kumar <rohit77428@gmail.com>
2024-04-10 01:15:45 +0530

    implemented myposts ans allposts routes
    Co-Authored-By: Amankumar1811 <164324447+Amankumar1811@users.noreply.github.com>
    Co-Authored-By: Narayan Ram Bhambhu <97403098+narayanbhambhu25@users.noreply.github.com>

--- server/routes/post.js
+++ server/routes/post.js
@@ -4,6 +4,29 @@ const mongoose = require("mongoose");
 const requirelogin = require("../middleware/requirelogin")
 const Post = mongoose.model("Post");
 
+router.get("/allpostes",(req,res)=>{
+    Post.find()
+    .populate("postedBy","_id name")
+    .then(posts=>{
+       
+        res.json({posts});
+    })
+    .catch(err=>{
+     console.log(err);
+    })
+})
+
+router.get("/mypost",requirelogin,(req,res)=>{
+    Post.find({postedBy:req.user._id})
+    .populate("postedBy","_id name")
+    .then(mypost=>{
+        res.json({mypost});
+    })
+    .catch(err=>{
+        console.log(err);
+       })
+
+})
 router.post("/createpost",requirelogin,(req,res)=>{
     const {title,body} = req.body
 
