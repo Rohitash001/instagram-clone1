32b1ea7e4297df93cf743e94bc3ea1740b021cc8
Rohitash Kumar <rohit77428@gmail.com>
2024-04-10 13:04:35 +0530

    add react app and components
    add react componanats for
    login
    signup
    profile
    home

---
+++ client/src/App.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import Navbar from './components/navbar';
+import "./App.css"
+import {BrowserRouter,Routes,Route} from 'react-router-dom'
+import Home from './components/screens/Home';
+import Login from './components/screens/Login';
+import Signup from './components/screens/Signup';
+import Profile from './components/screens/Profile';
+
+function App() {
+  return (
+    <>
+    <BrowserRouter>
+    <Navbar/>
+    
+    
+     
+   
+    
+   
+    <Routes>
+    <Route path="/" element={<Home/>} />
+     <Route path="/Home" element={<Home/>} />
+     <Route path="/Login" element={<Login/>} />
+     <Route path="/Signup" element={<Signup/>} />
+     <Route path="/Profile" element={<Profile/>} />
+    </Routes>
+    </BrowserRouter>
+    </>
+
+  )
+}
+
+export default App;
---
+++ client/src/components/navbar.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+const Navbar = ()=>{
+    return(
+        <nav>
+        <div className="nav-wrapper white">
+          <a href="/" className="brand-logo left">Instagram</a>
+          <ul id="nav-mobile" className="right ">
+            <li><a href="/Login">Login</a></li>
+            <li><a href="/Signup">signup</a></li>
+            <li><a href="/Profile">Profile</a></li>
+          </ul>
+        </div>
+      </nav>
+    )
+}
+
+export default Navbar;
