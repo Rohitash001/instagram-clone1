5db324face02e1f71de2b38dfc6c08d587cb914f
Rohitash Kumar <rohit77428@gmail.com>
2024-04-11 11:58:56 +0530

    styling of login and signup componant
    Co-Authored-By: Amankumar1811 <164324447+Amankumar1811@users.noreply.github.com>
    Co-Authored-By: Narayan Ram Bhambhu <97403098+narayanbhambhu25@users.noreply.github.com>

--- client/src/components/screens/Login.js
+++ client/src/components/screens/Login.js
@@ -1,9 +1,27 @@
 import React from "react";
-
+import {Link} from 'react-router-dom'
 
 const Login = ()=>{
 return (
-    <h1>Login</h1>
+   <div className="mycard">
+    <div className="card auth-card input-field">
+   <h2>instagram</h2>
+   <input
+   type="text"
+   placeholder="email"
+   />
+    <input
+   type="text"
+   placeholder="password"
+   />
+
+<a className="waves-effect waves-light btn-small #42a5f5 blue lighten-1">Login</a>
+
+<h5>
+    <Link to="/signup">Don't have an account ?</Link>
+</h5>
+    </div>
+</div>
 )
 }
 
--- client/src/components/screens/Signup.js
+++ client/src/components/screens/Signup.js
@@ -1,9 +1,32 @@
 import React from "react";
-
+import {Link} from 'react-router-dom'
 
 const Signup= ()=>{
 return (
-    <h1>Signup</h1>
+    <div className="mycard">
+    <div className="card auth-card input-field">
+   <h2>instagram</h2>
+   <input
+   type="text"
+   placeholder="name"
+   />
+   <input
+   type="text"
+   placeholder="email"
+   />
+   
+    <input
+   type="text"
+   placeholder="password"
+   />
+
+<a className="waves-effect waves-light btn-small #42a5f5 blue lighten-1">signup</a>
+
+<h5>
+    <Link to="/Login">Already have an account ?</Link>
+</h5>
+    </div>
+</div>
 )
 }
 
