8a769d9a73c8eca81d0ec22364a784f70c9ee675
Rohitash Kumar <rohit77428@gmail.com>
2024-04-12 02:47:37 +0530

    link sign up componant to signup route
    through react hooks make post request to signup route by signup react componant and getting response from
    route
    
    Co-Authored-By: Amankumar1811 <164324447+Amankumar1811@users.noreply.github.com>
    Co-Authored-By: Narayan Ram Bhambhu <97403098+narayanbhambhu25@users.noreply.github.com>

--- client/src/components/screens/Signup.js
+++ client/src/components/screens/Signup.js
@@ -1,7 +1,47 @@
-import React from "react";
-import {Link} from 'react-router-dom'
-
+import React,{useState} from "react";
+import {Link,useNavigate} from 'react-router-dom'
+import M from 'materialize-css'
 const Signup= ()=>{
+    const navigate = useNavigate();
+    const [name,setName]  =useState("");
+    const [password,setPassword]  =useState("");
+    const [email,setEmail]  =useState("");
+    const postData = ()=> {
+        const validateEmail = (email) => {
+            return email.match(
+              /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+            );
+          };
+
+           if(!validateEmail(email))
+           {
+            M.toast({html:"invalid email",classes:"#d32f2f red darken-2"})
+            return;
+           }
+          
+        fetch("/signup",{
+            method:"post",
+            headers:{
+                "Content-Type":"application/json"
+            },
+            body:JSON.stringify({
+                name,
+                password,
+                email
+            })
+        }).then(res=>res.json())
+        .then(data=>{
+           if(data.error)
+           {
+            M.toast({html:data.error,classes:"#d32f2f red darken-2"});
+           }
+           else{
+            M.toast({html:data.message,classes:"#388e3c green darken-2"})
+            navigate('/Login')
+           }
+        })
+    
+    }
 return (
     <div className="mycard">
     <div className="card auth-card input-field">
@@ -9,18 +49,32 @@ return (
    <input
    type="text"
    placeholder="name"
+   value={name}
+   onChange={(e)=>
+    setName(e.target.value)
+  }
    />
    <input
    type="text"
    placeholder="email"
+   value={email}
+   onChange={(e)=>
+    setEmail(e.target.value)
+  }
    />
    
     <input
    type="text"
    placeholder="password"
+   value={password}
+   onChange={(e)=>
+    setPassword(e.target.value)
+  }
    />
 
-<a className="waves-effect waves-light btn-small #42a5f5 blue darken-1">signup</a>
+<a className="waves-effect waves-light btn-small #42a5f5 blue darken-1"
+onClick={()=>postData()}
+>signup</a>
 
 <h5>
     <Link to="/Login">Already have an account ?</Link>
